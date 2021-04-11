ad9e7b0b0a76c37c674cba6463fef6a2a7830c19
enyuliu0414 <56370605+enyuliu0414@users.noreply.github.com>
2021-04-11 19:41:58 +1200

    router
    router update
    layout change

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 //Import Ant Design layout 
 import { Layout, Menu} from 'antd';
 
+//import router components
+import Home from './components/home'
+import About from './components/about'
+import Movie from './components/movie'
+
 const { Header, Content, Footer } = Layout;
 // import styles from './css/app.scss'
 
@@ -17,18 +22,28 @@ export default class App extends Component {
 		return (
       <HashRouter>
 	      <Layout className="layout">
-          <Header style={{backgroundColor:'#138D75'}}>
+          <Header style={{backgroundColor:'#202020'}}>
             <div className= "logo" />
-              <Menu style={{backgroundColor:'#138D75'}} theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">Home</Menu.Item>
-                <Menu.Item key="2">Movie</Menu.Item>
-                <Menu.Item key="3">About</Menu.Item>
-                <Button type="ghost" style={{float:'right',marginTop:'10px'}}>Login</Button>
-                <Button type="ghost" style={{float:'right',marginTop:'10px'}}>Sign up</Button>
+              <Menu style={{backgroundColor:'#202020'}} theme="dark" mode="horizontal" defaultSelectedKeys={[window.location.hash.split('/')[1]]}>
+                <Menu.Item key="home" style = {{backgroundColor:'#202020'}}>
+                  <Link to = "/home">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="movie" style = {{backgroundColor:'#202020'}}>
+                  <Link to = "/movie">Movie</Link>
+                </Menu.Item>
+                <Menu.Item key="about" style = {{backgroundColor:'#202020'}}>
+                  <Link to = "/about">About</Link>
+                </Menu.Item>
+                <Button type="link" style={{float:'right',marginTop:'10px',color:'white'}}>Login</Button>
+                <Button type="link" style={{float:'right',marginTop:'10px',color:'white'}}>Sign up</Button>
              </Menu>
           </Header>
          <Content style={{ padding: '0 50px',backgroundColor:'white' }}>
-           <div className="site-layout-content">Content</div>
+           <div className="site-layout-content">
+           <Route path = "/home" component = {Home}></Route>
+           <Route path = "/about" component = {About}></Route>
+           <Route path = "/movie" component = {Movie}></Route>
+           </div>
          </Content>
     <Footer style={{ textAlign: 'center' }}>Video streaming interface</Footer>
   </Layout>
